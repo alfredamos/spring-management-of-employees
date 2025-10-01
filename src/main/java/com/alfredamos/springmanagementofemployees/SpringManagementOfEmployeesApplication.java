1e0c40cfc047a68c54b1104b41e95c66c8c17a4e
Alfred Akintunde AMOS <52054578+alfredamos@users.noreply.github.com>
2025-10-01 21:27:52 +0100

    scaffold a spring backend project for employee management.

---
+++ src/main/java/com/alfredamos/springmanagementofemployees/SpringManagementOfEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.alfredamos.springmanagementofemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringManagementOfEmployeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringManagementOfEmployeesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alfredamos/springmanagementofemployees/SpringManagementOfEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfredamos.springmanagementofemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringManagementOfEmployeesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
